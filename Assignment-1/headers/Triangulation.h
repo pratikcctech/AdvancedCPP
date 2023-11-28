164c4ffe21fa826ea29d4f0815f5743d994f8d97
pratikcctech <149318512+pratikcctech@users.noreply.github.com>
2023-11-28 12:14:06 +0530

    Assignment-1 added

---
+++ Assignment-1/headers/Triangulation.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include "Triangle.h"
+#include "BBox.h"
+
+class Triangulation
+{
+public:
+    Triangulation(std::vector<Triangle> triangles);
+    ~Triangulation();
+
+    std::vector<Triangle> getTriangles();
+    void NoOfTriangles(std::vector<Triangle>& triangles);
+    BBox getBoundingBox();    
+    BBox getBBox();
+
+private:
+    std::vector<Triangle> mTriangles;
+    BBox mBBox;
+};
